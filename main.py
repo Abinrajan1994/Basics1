5e5905bc340aca244ea55ed9bc4e22384b788cbd
Abinrajan1994 <148305169+Abinrajan1994@users.noreply.github.com>
2025-09-13 10:40:02 +0530

    prime number between range

---
+++ main.py
@@ -0,0 +1,17 @@
+def isPrime(num):
+    count=0
+    for i in range(1,num+1):
+        if num%i==0:
+            count+=1
+    if count==2:
+        return True
+    else:
+        return False
+
+l1=[]
+s,e=map(int,input().split())
+count_prime=0
+for i in range(s,e+1):
+    if(isPrime(i)):
+        count_prime+=1
+print(count_prime)
